f50751355fb6886a2bf36d25e4bc416a5f950a35
Danyss09 <danielacaceresp4@gmail.com>
2025-02-04 01:02:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from routes.customer_controller import customer_controller
+from dotenv import load_dotenv
+import os
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
 
 app = Flask(__name__)
 
